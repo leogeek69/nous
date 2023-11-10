7e65a1b8b39ffaede577135fac6fee3ee1062b99
leogeek69 <145578837+leogeek69@users.noreply.github.com>
2023-11-10 09:25:39 +0000

    v 0.0.3 add c structure

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
     #include <stdio.h>
 
-    int main()
-{
-        printf("hello world\n");
-        printf("Bonjour à tous\n");
-
-
-    return 0;
-}
+    int main() {
+        int time = 6;
+        if (time < 12) {
+        printf("Passez une bonne journée");
+        } else if (time < 20) {
+            printf("Bonne soirée");
+        } else {
+            printf("Bonne nuit");
+        }      
+        return 0;
+    }    
